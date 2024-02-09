4c6c19af1085ee0c68e35d8da2dbed2743290aef
Shubhvrat <shubhvratkulkarni@gmail.com>
2024-02-09 19:02:08 +0530

    //made important  changes

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {  //Async await function forwards to index routes
   res.send('respond with a resource');
 });
 
