514ad8869446577a97274a3a37ce8e0a8d598ff6
Atharv Desai <atharvdesai2002@gmail.com>
2024-07-13 13:42:05 +0530

    Otherwa (#35)
    * /fixes/
    
    * /fixes/
    
    * /fixes/
    
    * /fixes Nave Bayers Logic Remaining/
    
    ---------
    
    Co-authored-by: Otherwa <67428572+Atharv2002@users.noreply.github.com>

--- public/js/script.js
+++ public/js/script.js
@@ -4,13 +4,11 @@ import FaceRecoHandler from './modules/FaceDetectionManager.js';
 
 // Entry point
 const videoElement = document.getElementById("video");
-const startBtn = document.getElementById("startAttendanceBtn");
-const stopBtn = document.getElementById("stopAttendanceBtn");
 const saveBtn = document.getElementById("saveAttendanceBtn");
 const tableBody = document.getElementById("attendanceTableBody");
 const stat = document.getElementById("Status");
 const count = document.getElementById("Count");
 
 // Initialize AttendanceManager
-new FaceRecoHandler(videoElement, startBtn, stopBtn, saveBtn, tableBody, stat, count)
+new FaceRecoHandler(videoElement, saveBtn, tableBody, stat, count)
 
